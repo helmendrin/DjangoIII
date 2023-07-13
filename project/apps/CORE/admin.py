7448c707820903c592857b34c7de3bce8a3bbdb8
Helen Echevarria <eche.helen@gmail.com>
2023-07-13 19:11:00 -0300

    admin y model creados

--- project/apps/CORE/admin.py
+++ project/apps/CORE/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from  . import models
 # Register your models here.
+admin.site.register (models.Estudiante)
+admin.site.register (models.Curso)
+admin.site.register (models.Profesor)
